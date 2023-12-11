16c5ed0a19eec72c39a18d283e106212f6a63908
shelby2770 <ajm.istiaque@g.bracu.ac.bd>
2023-12-11 23:13:18 +0600

    searchbar implementation done

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,11 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Event from "./Event";
 import { EventContext } from "../pages/Home";
 const Events = () => {
   const [obj, set_obj] = useContext(EventContext);
-  
+  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState([]);
+  const handleChange = async (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+    const updateEvent = await fetch("http://localhost:3000/events");
+    const getUpdateEvents = await fetch("http://localhost:3000/events");
+    const getUpdateEvents_json = await getUpdateEvents.json();
+    // console.log(getUpdateEvents_json);
+    const res = await set_obj(getUpdateEvents_json);
+    const newObj = obj.filter((item) =>
+      item.title.toLowerCase().includes(inputValue.toLocaleLowerCase())
+    );
+    set_obj(newObj);
+  };
   return (
-    <div className="mb-10">
+    <div className="mb-10 flex flex-col gap-4 items-center">
+      <div className="">
+        <form onSubmit={handleSubmit} className="flex flex-center join">
+          <input
+            className=" input input-bordered rounded-md join-item"
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Enter title"
+          />
+          <button
+            className="btn join-item rounded-r-md  bg-secondary_clr text-neutral-100"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {obj.map((item) => (
           <Event key={item._id} item={item}></Event>
